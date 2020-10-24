a2d9d0eb572135794cfcbc2033b146869647cfcb
Наранхүү <codesaur@gmail.com>
2020-10-24 20:49:55 +0800

    Update Mail.php

--- Base/Mail.php
+++ Base/Mail.php
@@ -43,7 +43,7 @@ class Mail extends Base
         if (\mail($recipient ?? $this->to, $subject, \base64_encode($this->message), $header)) {
             return true;
         } else {
-            return false;
+            throw new \Exception(\error_get_last()['message']);
         }
     }
 }
