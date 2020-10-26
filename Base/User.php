b8a294f3cb17435896438278dc75b85d792de08d
Наранхүү <codesaur@gmail.com>
2020-10-26 13:36:06 +0800

    Update User.php

--- Base/User.php
+++ Base/User.php
@@ -10,23 +10,24 @@ class Authentication
 class User extends Base
 {
     private $_account;
-    private $_organization;
+    private $_organizations;
     private $_role_permissions;
     
     private $_status = Authentication::Unset;
     
-    public function login(array $account, array $org, array $roles)
+    public function login(array $account, array $organizations, array $role_permissions)
     {
-        $this->_account = $account;
-        $this->_organization = $org;
-        $this->_role_permissions = $roles;       
-        
-        $this->_status = Authentication::Login;
-        
         if (isset($this->_account['id'])
                 && \is_int($this->_account['id'])) {
+            $this->_status = Authentication::Login;
             \putenv(_ACCOUNT_ID_ . "={$this->_account['id']}");
+        } else {
+            return;
         }
+
+        $this->_account = $account;
+        $this->_organizations = $organizations;
+        $this->_role_permissions = $role_permissions;
     }
 
     public function logout()
@@ -49,16 +50,21 @@ class User extends Base
         return $this->_status == Authentication::Locked;
     }
     
-    public function account($index)
+    public function account($key)
     {
-        return $this->_account[$index] ?? null;
+        return $this->_account[$key] ?? null;
     }
     
-    public function organization($index)
+    public function organization($key)
     {
-        return $this->_organization[$index] ?? null;
+        return $this->_organizations[0][$key] ?? null;
     }
     
+    public function organizations() : ?array
+    {
+        return $this->_organizations;
+    }
+
     public function is($role) : bool
     {        
         if (isset($this->_role_permissions['system_coder'])) {
