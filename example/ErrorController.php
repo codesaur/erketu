419a4a5ba2f6a67121667ba43a34f93080e47705
Наранхүү <codesaur@gmail.com>
2021-01-09 23:04:23 +0800

    Update ErrorController.php

--- example/ErrorController.php
+++ example/ErrorController.php
@@ -7,17 +7,16 @@ class ErrorController extends Controller
 {
     public function error(string $message, int $status)
     {
-        // credits to template
-        // Author: Tibix
-        // August 27, 2019
-        // NEON - 404 PAGE NOT FOUND
-        
         if ( ! \headers_sent()) {
             \http_response_code($status);
         }
         
         \error_log("Error[$status]: $message");
         
+        // credits to template
+        // Author: Tibix
+        // August 27, 2019
+        // NEON - 404 PAGE NOT FOUND
         (new Template(\dirname(__FILE__) . '/neon.html', array('message' => $message)))->render();
     }
 }
