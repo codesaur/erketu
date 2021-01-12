54174e4495e82ab3f28d5932df628a1ea7f16af8
Наранхүү <codesaur@gmail.com>
2021-01-12 15:23:48 +0800

    Update ExampleRouter.php

--- example/ExampleRouter.php
+++ example/ExampleRouter.php
@@ -6,8 +6,10 @@ class ExampleRouter extends Router
 {
     function __construct()
     {
-        $this->map('/post-or-put', 'post_put@erketu\\Example\\ExampleController', ['methods' => ['POST', 'PUT']]);
-        $this->map('/hello/:firstname', 'hello@erketu\\Example\\ExampleController', ['filters' => ['firstname' => '(\w+)']]);
+        $namespace = __NAMESPACE__;
+        
+        $this->map('/post-or-put', "post_put@$namespace\\ExampleController", ['methods' => ['POST', 'PUT']]);
+        $this->map('/hello/:firstname', "hello@$namespace\\ExampleController", ['filters' => ['firstname' => '(\w+)']]);
         
         $this->any('/echo/:singleword', function($req) { echo $req->params->singleword; });
     }
