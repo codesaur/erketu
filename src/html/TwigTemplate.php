a20cdde7a168ed1e6e57a4e36d16d96bb2d2e02c
Наранхүү <codesaur@gmail.com>
2021-01-13 18:54:49 +0800

    Update TwigTemplate.php

--- src/html/TwigTemplate.php
+++ src/html/TwigTemplate.php
@@ -16,36 +16,21 @@ class TwigTemplate extends FileTemplate
         
         $this->_twig = new Environment(new ArrayLoader(), array('autoescape' => false));
         
-        $this->addFilter('int', function($variable) { return \intval($variable); });
-        $this->addFilter('json_decode', function($data, $param = true) { return \json_decode($data, $param); });
+        $this->_twig->addFilter(new TwigFilter('int', function($variable) { return \intval($variable); }));
+        $this->_twig->addFilter(new TwigFilter('json_decode', function($data, $param = true) { return \json_decode($data, $param); }));
  
-        $this->addFunction('script', function($src, $attr = 'defer') {
+        $this->_twig->addFunction(new TwigFunction('script', function($src, $attr = 'defer') {
             return new Markup('<script ' . $attr . ' src="' . $src . '"></script>', 'UTF-8');
-        });
+        }));
         
-        $this->addFunction('stylesheet', function($href, $attr = null) {
+        $this->_twig->addFunction(new TwigFunction('stylesheet', function($href, $attr = null) {
             return new Markup('<link href="' . $href . '" rel="stylesheet" type="text/css" ' . $attr . '>', 'UTF-8');
-        });
+        }));
     }
     
     public function &twig() : Environment
     {
         return $this->_twig;
-    }
-    
-    public function addGlobal($name, $value)
-    {
-        $this->_twig->addGlobal($name, $value);
-    }
-
-    public function addFilter(string $name, $callable = null, array $options = [])
-    {
-        $this->_twig->addFilter(new TwigFilter($name, $callable, $options));
-    }
-        
-    public function addFunction(string $name, $callable = null, array $options = [])
-    {
-        $this->_twig->addFunction(new TwigFunction($name, $callable, $options));
     }
 
     protected function compile(string $html) : string
